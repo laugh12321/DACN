7c0d4f6d80e8d9537401cafd8bb90d2c8ff420d3
Laugh <laugh12321@vip.qq.com>
2021-03-10 11:48:43 +0800

    Update attention.py

--- src/model/attention.py
+++ src/model/attention.py
@@ -104,3 +104,62 @@ class Position_attention(tf.keras.layers.Layer):
         outputs = self.gamma * outputs + inputs
 
         return outputs
+
+
+def cbam_block(cbam_feature, ratio=8):
+    """
+    Contains the implementation of Convolutional Block Attention Module(CBAM) block.
+    As described in https://arxiv.org/abs/1807.06521.
+    """
+
+    cbam_feature = channel_attention(cbam_feature, ratio)
+    cbam_feature = spatial_attention(cbam_feature)
+
+    return cbam_feature
+
+
+def channel_attention(input_feature, ratio=8):
+
+    channel = input_feature.get_shape().as_list()[-1]
+
+    shared_layer_one = tf.keras.layers.Dense(channel//ratio,
+                                             activation='relu',
+                                             kernel_initializer='he_normal',
+                                             use_bias=True,
+                                             bias_initializer='zeros')
+    shared_layer_two = tf.keras.layers.Dense(channel,
+                                             kernel_initializer='he_normal',
+                                             use_bias=True,
+                                             bias_initializer='zeros')
+
+    avg_pool = tf.keras.layers.GlobalAveragePooling3D()(input_feature)    
+    avg_pool = tf.keras.layers.Reshape((1,1,1,channel))(avg_pool)
+    avg_pool = shared_layer_one(avg_pool)
+    avg_pool = shared_layer_two(avg_pool)
+
+    max_pool = tf.keras.layers.GlobalMaxPooling3D()(input_feature)
+    max_pool = tf.keras.layers.Reshape((1,1,1,channel))(max_pool)
+    max_pool = shared_layer_one(max_pool)
+    max_pool = shared_layer_two(max_pool)
+
+    cbam_feature = tf.keras.layers.Add()([avg_pool,max_pool])
+    cbam_feature = tf.keras.layers.Activation('sigmoid')(cbam_feature)
+
+    return tf.keras.layers.multiply([input_feature, cbam_feature])
+
+
+def spatial_attention(input_feature):
+    kernel_size = 7
+
+    channel = input_feature.get_shape().as_list()[-1]
+    cbam_feature = input_feature
+
+    avg_pool = tf.keras.layers.Lambda(lambda x: tf.keras.backend.mean(x, axis=-1, keepdims=True))(cbam_feature)
+    max_pool = tf.keras.layers.Lambda(lambda x: tf.keras.backend.max(x, axis=-1, keepdims=True))(cbam_feature)
+    concat = tf.keras.layers.Concatenate(axis=-1)([avg_pool, max_pool])
+    cbam_feature = tf.keras.layers.Conv3D(filters = 1, kernel_size=kernel_size,
+                                          strides=1, padding='same', activation='sigmoid',
+                                          kernel_initializer='he_normal', use_bias=False)(concat)	
+        
+    return tf.keras.layers.multiply([input_feature, cbam_feature])
+    
